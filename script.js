01ce79b3d0f28efb35a4a5de8e5b583a4827fc2a
Khijar Sheikh <120732221+Khijar21@users.noreply.github.com>
2023-05-23 14:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+// function removeColor(){
+// 	let x = document.getElementById("colorSelect")
+// 		x.remove(x.selectedIndex);
+// }
+
+function removecolor()
+{
+let x=document.getElementById('colorSelect');
+	x.remove(x.selectedIndex);
+}
